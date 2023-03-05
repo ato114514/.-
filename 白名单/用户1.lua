6b5bd301da32089eec8e69fe862d46692c792050
IIIlllIlIIIllllllIIIllllllIII <124188542+IIIlllIlIIIllllllIIIllllllIII@users.noreply.github.com>
2023-03-05 10:16:57 +0800

    Update 用户1.lua

--- 白名单/用户1.lua
+++ 白名单/用户1.lua
@@ -1,1 +1,9 @@
-   loadstring(game:HttpGet(('https://raw.githubusercontent.com/IIIlllIlIIIllllllIIIllllllIII/.-/main/%E7%99%BD%E5%90%8D%E5%8D%95/%E7%94%A8%E6%88%B72.txt')))()
+spawn(function()
+game.StarterGui:SetCore("SendNotification", {
+		Title = "白";
+		Text = "正在验证白名单";
+		Icon = nil;
+		Duration = 3
+	})
+	end) 
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/IIIlllIlIIIllllllIIIllllllIII/.-/main/%E7%99%BD%E5%90%8D%E5%8D%95/%E7%94%A8%E6%88%B72.txt')))()
